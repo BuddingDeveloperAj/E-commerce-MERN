6927e27c945621f400a8c6b4c7831e24afbeb92c
Ajay Ravi <107797885+BuddingDeveloperAj@users.noreply.github.com>
2022-10-13 00:55:10 +0530

    Update Braintree.js

--- BackEnd/controllers/Braintree.js
+++ BackEnd/controllers/Braintree.js
@@ -2,9 +2,9 @@ var braintree = require("braintree");
 
 var gateway = new braintree.BraintreeGateway({
     environment:  braintree.Environment.Sandbox,
-    merchantId:   'd4h5yzhnpkfdgh5m',
-    publicKey:    'brjqwf59qwrxsgtn',
-    privateKey:   '8d4ed7a9de7caa7526949dd0a2a0e194'
+    merchantId:   'YOUR ID',
+    publicKey:    'YOUR KEY',
+    privateKey:   'YOUR KEY'
 });
 
 exports.getToken = (req, res) => {
